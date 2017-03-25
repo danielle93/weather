2e834036dcd8ff1e42a4a39028b8bba078432e91
Danielle Roberson <danielle.roberson@icloud.com>
2017-03-25 00:34:27 -0700

    Having issues getting my weather.forecast[1] to work.
    weather.forecast[1] loads icon, and day but not temp or currently.

--- js/scripts.js
+++ js/scripts.js
@@ -30,34 +30,56 @@ $.simpleWeather({
             $('section').addClass('cloudy');
             $('.currently').text('Depressing & grey as heck');
         }
-        if (weather.code == 23 || weather.code == 24) {
-            $('section').addClass('windy');
-        }
-        if (weather.code >= 30 && weather.code <= 36) {
-            $('section').addClass('sunny');
-        }
-        if (weather.code == 36) {
-            $('section').addClass('hot');
-        }
-        if (weather.code == 35 || weather.code >= 12 && weather.code <= 19 || weather.code == 7 || weather.code >= 40 && weather.code <= 44 || weather.code == 46) {
-            $('section').addClass('snow');
-        }
-        if (weather.code == 25) {
-            $('section').addClass('cold');
-        }
-        if (weather.code == 39 || weather.code == 40 || weather.code >= 5 && weather.code <= 13) {
-            $('section').addClass('rainy');
-            $('.currently').text('Water falling from the Hecking Sky! HECK!');
-        }
-        if (weather.code == 3 || weather.code == 4 || weather.code == 37 || weather.code == 38 || weather.code == 45 || weather.code == 47) {
-            $('section').addClass('thunderstorms');
-        }
-        if (weather.code >= 0 && weather.code <= 2) {
-            $('section').addClass('severe');
-        }
-        if (weather.code == 27 || weather.code == 29 || weather.code == 31 || weather.code == 33) {
-            $('section').addClass('night');
-        }
+        if (weather.code >= 19 && weather.code <= 23 || weather.code == 26 || weather.code == 28 || weather.code == 44) {
+                $('section').addClass('cloudy');
+                $('.currently').text('Just a bunch of heckin clouds in the sky.');
+            }
+            if (weather.code == 23 || weather.code == 24) {
+                $('section').addClass('windy');
+                $('.currently').text('Wind. Heck. Wind is not frend.');
+
+            }
+            if (weather.code >= 30 && weather.code <= 36) {
+                $('section').addClass('sunny');
+                $('.currently').text('Heck. So beautiful. Soak up that sun, frend.');
+
+            }
+            if (weather.code == 36) {
+                $('section').addClass('hot');
+                $('.currently').text('Heckin hot!');
+            }
+            if (weather.code == 35 || weather.code >= 12 && weather.code <= 19 || weather.code == 7 || weather.code >= 40 && weather.code <= 44 || weather.code == 46) {
+                $('section').addClass('Snow');
+                $('.currently').text('Heckin snow doggo');
+
+            }
+            if (weather.code == 25) {
+                $('section').addClass('cold');
+                $('.currently').text('Stay warm pupper!');
+
+            }
+            if (weather.code == 39 || weather.code == 40 || weather.code >= 5 && weather.code <= 13) {
+                $('section').addClass('rainy');
+                $('.currently').text('Water falling from the hecking sky?!');
+            }
+            if (weather.code == 3 || weather.code == 4 || weather.code == 37 || weather.code == 38 || weather.code == 45 || weather.code == 47) {
+                $('section').addClass('thunderstorms');
+                $('.currently').text('Thunderstorms doin a<br>heckin spook!');
+
+            }
+            if (weather.code >= 0 && weather.code <= 2) {
+                $('section').addClass('severe');
+                $('.currently').text('Severe weather doin me a heck concern');
+            }
+            if (weather.code == 27 || weather.code == 29 || weather.code == 31 || weather.code == 33) {
+                $('section').addClass('night');
+                $('.currently').text('Heck why u still up frend? Go do a heckin sleep!');
+            }
+            
+            if (weather.code == 29 || weather.code == 30){
+                $('section').addClass('partlyCloudy');
+                $('.currently').text('Clouds doin the sun a heckin bamboozle.');
+            }
     }
     , error: function (error) {
         // Show if weather cannot be retreived
@@ -168,7 +190,7 @@ var getWeather = function (location) {
             }
             if (weather.code == 27 || weather.code == 29 || weather.code == 31 || weather.code == 33) {
                 $('section').addClass('night');
-                $('.currently').text('Heckin snow doggo');
+                $('.currently').text('Heck why u still up frend? Go do a heckin sleep!');
             }
             
             if (weather.code == 29 || weather.code == 30){
